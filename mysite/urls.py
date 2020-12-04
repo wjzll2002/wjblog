985967c1e83e383c50060bc693f9b837817232dd
jian.wang <jiang.wang@oibp.com>
2020-12-04 15:53:07 +0800

    whitenoise bug

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', include('blog.urls'))
     # path('blog/', include('blog.urls'))
 ]
